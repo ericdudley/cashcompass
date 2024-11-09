93907da7afb6d4b57273016b8aa98a6aa863b121
Eric Dudley <17283326+ericdudley@users.noreply.github.com>
2024-11-08 21:59:01 -0800

    fix: Layout

--- src/routes/categories/+page.svelte
+++ src/routes/categories/+page.svelte
@@ -7,10 +7,10 @@
 </script>
 
 <div class="container max-w-4xl mx-auto flex flex-wrap justify-center gap-4">
-	<div class="flex-1">
+	<div class="flex-1 min-w-96">
 		<CategoryForm bind:label={prefix} />
 	</div>
-	<div class="flex-1">
+	<div class="flex-1 min-w-96">
 		<CategoryList {prefix} />
 	</div>
 </div>
